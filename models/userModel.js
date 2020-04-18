b66e8662c48755396cf45e72c6dc2577c0fae321
rajaavsk <rajaavsk@users.noreply.github.com>
2020-04-18 13:59:16 +0530

    Restful API implementation with node, express and mongo

---
+++ models/userModel.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+// Setup schema
+var userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  gender: String,
+  phone: String,
+  address: String,
+  create_date: {
+    type: Date,
+    default: Date.now
+  }
+});
+// Export user model
+var User = module.exports = mongoose.model('user', userSchema);
+module.exports.get = function(callback, limit) {
+  User.find(callback).limit(limit);
+}
